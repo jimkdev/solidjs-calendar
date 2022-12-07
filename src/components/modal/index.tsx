4334b837a2df45b1cbdefdc80dd3105e87181ab1
Jim <simou@proton.me>
2022-12-07 18:12:09 +0200

    make select to hourly

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -50,12 +50,19 @@ export const Modal: Component<ReservationProps> = (props: ReservationProps) => {
                     }}
                   >
                     <option value="7">07:00</option>
+                    <option value="8">08:00</option>
                     <option value="9">09:00</option>
+                    <option value="10">10:00</option>
                     <option value="11">11:00</option>
+                    <option value="12">12:00</option>
                     <option value="13">13:00</option>
+                    <option value="14">14:00</option>
                     <option value="15">15:00</option>
+                    <option value="16">16:00</option>
                     <option value="17">17:00</option>
+                    <option value="18">18:00</option>
                     <option value="19">19:00</option>
+                    <option value="20">20:00</option>
                     <option value="21">21:00</option>
                   </select>
                 </td>
@@ -75,12 +82,19 @@ export const Modal: Component<ReservationProps> = (props: ReservationProps) => {
                     }}
                   >
                     <option value="7">07:00</option>
+                    <option value="8">08:00</option>
                     <option value="9">09:00</option>
+                    <option value="10">10:00</option>
                     <option value="11">11:00</option>
+                    <option value="12">12:00</option>
                     <option value="13">13:00</option>
+                    <option value="14">14:00</option>
                     <option value="15">15:00</option>
+                    <option value="16">16:00</option>
                     <option value="17">17:00</option>
+                    <option value="18">18:00</option>
                     <option value="19">19:00</option>
+                    <option value="20">20:00</option>
                     <option value="21">21:00</option>
                   </select>
                 </td>
